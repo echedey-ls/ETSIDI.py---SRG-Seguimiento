43bf6f51b1cf6288c25adf0d8463f8dc74f64f14
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-09-13 13:23:55 +0200

    Initial commit

---
+++ Action-monitoring.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+"""
+Author: Echedey Luis Álvarez
+Date: 13/09/2021
+Project: Action-monitoring, for ETSIDI.py
+
+File: Action-monitoring
+
+Abstract: main file
+
+Sources:
+
+"""
+
+import sys, os
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
